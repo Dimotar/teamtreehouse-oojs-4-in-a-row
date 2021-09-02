8a688d7afd5a69e8a85bd3949173e053402532d2
Jason <jasonp670@gmail.com>
2021-09-02 13:46:00 -0600

    create Game class, create players method

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,19 @@
 class Game {
-    
+    constructor() {
+        this.board = new Board();
+        this.players = this.createPlayers();
+        this.ready = false;
+    }
+
+
+    /**
+     * Creates two player objects
+     * @returns {array} An array of two player objects.
+     */
+    createPlayers() {
+        const player1 = new Player('Player 1', 1, '#e15258', true);
+        const player2 = new Player('Player 2', 2, '#e59a13');
+
+        return [player1, player2];
+    }
 }
